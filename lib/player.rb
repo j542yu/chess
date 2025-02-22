2d250ed96e77750ec89a8f6ee2027bf0f198133d
judywsl <j542yu@uwaterloo.ca>
2025-02-21 22:36:57 -0500

    Add Player#make_move and helper methods
    * Implement translators for indices to
      typical chess board labels (ex. e4) and
      vice versa to handle user input
    * Player#make_move takes optional parameter
      in case Game calls again
      when player's move is illegal

--- lib/player.rb
+++ lib/player.rb
@@ -9,11 +9,27 @@ class HumanPlayer
 
   def initialize(color, player_num = 0)
     @player_num = player_num # greet differently if two human players VS human against computer
-    @name = ask_name
+    @name = ask_player_name
     @color = color
   end
 
-  def ask_name
+  def make_move(board, previously_invalid: false)
+    puts 'Your previous attempt was an illegal move. Try again.' if previously_invalid
+
+    puts "#{name}, it's your turn!"
+
+    piece_name = ask_for_piece_name
+    piece = clarify_piece(piece_name, board)
+    move = ask_for(:move)
+
+    move_translator(move)
+
+    [piece, move]
+  end
+
+  private
+
+  def ask_player_name
     print @player_num.positive? ? "Hey, player #{@player_num}!" : 'Hey you, yes you the human.'
     print "What's your name? Psst... just press enter if you want a random name :>\n=> "
 
@@ -23,5 +39,60 @@ class HumanPlayer
     name = RANDOM_NAMES.sample
     puts "Alright then, you'll be #{name} (sorry...)."
     name
+  end
+
+  def ask_for_piece_name
+    print "Enter the name of the piece to move:\n=> "
+
+    input = nil
+    loop do
+      input = gets.chomp.capitalize.to_sym
+      break if piece_valid?(input)
+
+      print "Oop, that's not a piece in chess.\n" \
+            'Your input should be any of the following:' \
+            "'Rook', 'Knight', 'Bishop', 'Queen', 'King', 'Pawn'\n=> "
+    end
+  end
+
+  def clarify_piece(piece_name, board) # rubocop:disable Metrics/AbcSize
+    pieces = board.ally_pieces(@color).select { |ally_piece| ally_piece.class.name.to_sym == piece_name }
+    return if pieces.size == 1
+
+    print "There's multiple #{piece_name}s: "
+    pieces.each do |piece|
+      alphanum_position = indices_to_alphanum(piece.position)
+      print "#{alphanum_position} "
+    end
+
+    print "Which piece would you like to move?\n=>"
+    position = ask_for(:position)
+    pieces.find { |piece| piece.position == position }
+  end
+
+  def piece_valid?(input)
+    %i[Rook Knight Bishop Queen King Pawn].include?(input)
+  end
+
+  def ask_for(input)
+    print "Enter your #{input}:\n=> "
+    loop do
+      input = gets.chomp.chars
+      return input if move_valid?(input)
+
+      print "Oop, that's invalid. Your input should be a letter followed by a number, like 'e4'. Try again please!\n=> "
+    end
+  end
+
+  def move_valid?(input)
+    input.size == 2 && /[a-h]/.include?(input[0]) && (1..8).include?(input[1])
+  end
+
+  def alphanum_to_indices(alphanum)
+    [ord(alphanum[0]) - ord('a'), alphanum[1].to_i - 1]
+  end
+
+  def indices_to_alphanum(indices)
+    "#{(indices[0] + ord('a')).chr}#{indices[1] + 1}"
   end
 end
